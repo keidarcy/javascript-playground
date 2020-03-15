b0f2ed644f0b2e9e21a00d8c0677eb498222ea4f
XING YAHAO <keidarcy.1015@gmail.com>
2020-03-15 22:55:43 +0900

    nuxt axios

--- nuxt/Fire-Water/pages/index.vue
+++ nuxt/Fire-Water/pages/index.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
     <h1>event</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
+  components: { EventCard },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3001/events')
+      .then((response) => {
+        return {
+          events: response.data
+        }
+      })
+      .catch((e) => {
+        error({ statusCode: 503, message: 'Unable to fetch events api' })
+      })
+  },
   head() {
     return {
       title: 'Event listing events'
