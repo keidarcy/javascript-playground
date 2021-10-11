69c28709e591d06e338cfbc9b92db4347d7a3a2e
Xing Yahao <keidarcy.1015@gmail.com>
2021-10-11 13:15:00 +0900

    add link for mock class

--- express/A.test.js
+++ express/A.test.js
@@ -2,6 +2,7 @@ const { A } = require('./A.js');
 const { B } = require('./B.js');
 
 const result = 2;
+// https://stackoverflow.com/questions/47093028/how-to-mock-the-instance-function-of-another-class-using-jest
 jest.mock('./B.js', () => {
   const mBInstance = { c: jest.fn(() => result) };
   const mB = jest.fn(() => mBInstance);
